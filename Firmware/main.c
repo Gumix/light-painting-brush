f8c4cad47d2b211cac997ad2289251c3273aa569
Ilya Verbin <iverbin@gmail.com>
2021-10-24 18:00:00 +0300

    Use 3 PWM channels for setting RGB intensities

--- Firmware/main.c
+++ Firmware/main.c
@@ -2,9 +2,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define RED   OCR0A	// PB2
+#define GREEN OCR1A	// PB3
+#define BLUE  OCR1B	// PB4
+
 static void init(void)
 {
-	DDRB = 0b11111111;
+	// PB0 - Input, PB2-PB4 - Output
+	DDRB  = 0b00011100;
+
+	// Timer/Counter0: Fast PWM mode, Top = 0xFF, No prescaling
+	TCCR0A = _BV(COM0A1) | _BV(WGM01) | _BV(WGM00);
+	TCCR0B = _BV(CS00);
+
+	// Timer/Counter1: Fast PWM mode (8 bit), Top = 0xFF, No prescaling
+	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM12) | _BV(WGM10);
+	TCCR1B = _BV(CS10);
 }
 
 int main()
@@ -13,11 +26,17 @@ int main()
 
 	while (true)
 	{
-		PORTB = _BV(PB2);
+		RED   = 255;
+		GREEN = 64;
+		BLUE  = 64;
 		_delay_ms(1000);
-		PORTB = _BV(PB3);
+		RED   = 64;
+		GREEN = 255;
+		BLUE  = 64;
 		_delay_ms(1000);
-		PORTB = _BV(PB4);
+		RED   = 64;
+		GREEN = 64;
+		BLUE  = 255;
 		_delay_ms(1000);
 	}
 }
